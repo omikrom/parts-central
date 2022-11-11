ebd335aa5d014dc9c5b8fb5529430a2b0632af04
Richard Lamnea <omikrom2@gmail.com>
2022-11-11 16:01:05 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -1,37 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const path = require("path");
+const router = require("./routes/router.js");
 
+app.use(cors());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-/*const mariadb = require("mariadb");
-const pool = mariadb.createPool({
-  host: "localhost",
-  user: "parts_admin",
-  password: "8~vlb632S",
-  database: "parts_central_new",
-  connectionLimit: 5,
-});*/
+app.use(express.static(path.join(__dirname, "public")));
 
-console.log("test");
-
-app.get("/", (req, res) => {
-  res.send("index of app");
-});
-
-/*app.get("/test", async (req, res) => {
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    //const rows = await conn.query("SELECT * FROM db.customers");
-    const rows = await conn.query("SELECT * FROM customers");
-    res.send("a test query" + rows);
-  } catch (err) {
-    throw err;
-  } finally {
-    if (conn) return conn.end();
-  }
-});*/
-
+app.use("/api", router);
 app.listen(port, () => console.log(`Server running on port ${port}`));
